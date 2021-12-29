a1fc626eec4c6cfbc18b7fcbe6b6e1d160d7240b
Ashwin sawkare <asawakare@gammastack.com>
2021-12-29 18:25:14 +0530

    scheduled the email with celery beat

--- django_celery_project/celery.py
+++ django_celery_project/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
 
@@ -16,6 +17,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.beat_schedule = {
     #add task
+    'send-mail-every-day-at-7':{
+        'task':'email_sending.task.send_mail_func',
+        'schedule':crontab(hour=18, minute=19),
+      #  'args':()
+    }
+    
 }
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
